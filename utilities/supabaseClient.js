42b661bd6f085af1315e5caed654dc51339b87e4
Efrat Hubara <efrathugim@gmail.com>
2025-12-15 08:27:19 +0200

    supabase

--- utilities/supabaseClient.js
+++ utilities/supabaseClient.js
@@ -1,5 +1,5 @@
 // supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+const { createClient } = window.supabase;
 
 const supabaseUrl = 'https://bmrtobuvjuycnvvfmgvf.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtcnRvYnV2anV5Y252dmZtZ3ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1ODQ5MDUsImV4cCI6MjA2NjE2MDkwNX0.VhoKIR_nb6lyu_05CEsVT8G_c90chKTX8v__5QA-A-s';
